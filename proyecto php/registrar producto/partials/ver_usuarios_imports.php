df1f2062f648279fcb87a7d904650d0851962594
DonJavier00 <94648473+DonJavier00@users.noreply.github.com>
2022-01-18 13:34:09 -0500

    Tablas de admin y empleados agregadas

--- proyecto php/registrar producto/partials/ver_usuarios_imports.php
+++ proyecto php/registrar producto/partials/ver_usuarios_imports.php
@@ -4,7 +4,78 @@
 
       <div class="ventana_ver_administradores">
      <a href="#" class="btn-close_ver_proveedor">X</a>
-     <h1>ver administradores</h1>
+     <h1>Tabla Administradores</h1><br>
+
+     <table class="tabla_administrador">
+        <tr>
+            <th class="documento_administrador_tabla">
+              N°
+            </th>
+            <th>    
+            N°Documento/NIT
+            </th>
+            <th>
+                Correo
+            </th>
+            <th>
+                NombreAdmin
+            </th>
+            <th>
+                ApellidoAdmin
+            </th>
+            <th>
+                TelefonoAdmin
+            </th>
+            <th>
+                EstadoAdmin
+            </th>
+            <th>
+              Editar
+      
+            </th>
+        </tr>
+
+        <tr>
+            <td>1</td>
+            <td>123456</td>
+            <td>ferrogameza@gmail.com</td>
+            <td>Juan Manuel</td>
+            <td>Perez Maldonado</td>
+            <td>3206786543</td>
+            
+            <td>
+                <select class="estado_administrador">
+                    <option disabled>Seleccione un estado</option>
+                    <option>Activo</option>
+                    <option>Inactivo</option>
+                </select>
+            </td>
+            <td> <a href="" id="editar_administrador" 
+                class="link_editar_administrador"><i
+                  class="fas fa-edit"></i>Editar</a></td>
+        </tr>
+
+        <tr>
+            <td>2</td>
+            <td>123457</td>
+            <td>maria13@gmail.com</td>
+            <td>Maria Isabel</td>
+            <td>Martinez Curtidor</td>
+            <td>3117450987</td>
+            
+            <td>
+                <select class="estado_administrador">
+                    <option disabled>Seleccione un estado</option>
+                    <option>Activo</option>
+                    <option>Inactivo</option>
+                </select>
+            </td>
+            <td> <a href="" id="editar_administrador" 
+                class="link_editar_administrador"><i
+                  class="fas fa-edit"></i>Editar</a></td>
+     </tr>
+     
+    </table>
     </div>
   </div>
 
@@ -24,7 +95,156 @@
 
       <div class="ventana_ver_usuarios">
      <a href="#" class="btn-close_ver_proveedor">X</a>
-     <h1>ver usuarios o empleados</h1>
+     <h1>Tabla Empleados</h1><br>
+
+     <table class="tabla_empleados">
+        <tr>
+            <th class="documento_empleado_tabla">
+              N°
+            </th>
+            <th>
+                N°Documento/NIT
+            </th>
+            <th>
+                Correo
+            </th>
+            <th>
+                NombreEmpleado
+            </th>
+            <th>
+                ApellidoEmpleado
+            </th>
+            <th>
+                TelefonoEmpleado
+            </th>
+            <th>
+                EstadoEmpleado
+            </th>
+            <th>
+              Editar
+            </th>
+        </tr>
+        
+        <tr>  
+            <td>1</td>
+            <td>123456</td>
+            <td>javier123@gmail.com</td>
+            <td>Javier</td>
+            <td>Sanchez</td>
+            <td>123456789</td>
+            
+            <td>
+                <select class="estado_empleado">
+                    <option disabled>Seleccione un estado</option>
+                    <option>Activo</option>
+                    <option>Inactivo</option>
+                </select>
+            </td>
+            <td> <a href="" id="editar_empleado" 
+                class="link_editar_empleado"><i
+                  class="fas fa-edit"></i>Editar</a></td>
+
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>01234567</td>
+            <td>brayan04@gmail.com</td>
+            <td>Brayan</td>
+            <td>Mendivelso</td>
+            <td>834892340</td>
+            
+            <td>
+                <select class="estado_empleado">
+                    <option disabled>Seleccione un estado</option>
+                    <option>Activo</option>
+                    <option>Inactivo</option>
+                </select>
+            </td>
+            <td> <a href="" id="editar_empleado" 
+                class="link_editar_empleado"><i
+                  class="fas fa-edit"></i>Editar</a></td>
+
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>8238218</td>
+            <td>alejo123@gmail.com</td>
+            <td>Alejandro</td>
+            <td>Mesa</td>
+            <td>123456232</td>
+            
+            <td>
+                <select class="estado_empleado">
+                    <option disabled>Seleccione un estado</option>
+                    <option>Activo</option>
+                    <option>Inactivo</option>
+                </select>
+            </td>
+            <td> <a href="" id="editar_empleado" 
+                class="link_editar_empleado"><i
+                  class="fas fa-edit"></i>Editar</a></td>
+
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>13434234</td>
+            <td>karen1343@gmail.com</td>
+            <td>Karen</td>
+            <td>Estepa</td>
+            <td>13433222</td>
+            
+            <td>
+                <select class="estado_empleado">
+                    <option disabled>Seleccione un estado</option>
+                    <option>Activo</option>
+                    <option>Inactivo</option>
+                </select>
+            </td>
+            <td> <a href="" id="editar_empleado" 
+                class="link_editar_empleado"><i
+                  class="fas fa-edit"></i>Editar</a></td>
+
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>34234234</td>
+            <td>luisa9454@gmail.com</td>
+            <td>Luisa</td>
+            <td>Salamanca</td>
+            <td>134353425</td>
+            
+            <td>
+                <select class="estado_empleado">
+                    <option disabled>Seleccione un estado</option>
+                    <option>Activo</option>
+                    <option>Inactivo</option>
+                </select>
+            </td>
+            <td> <a href="" id="editar_empleado" 
+                class="link_editar_empleado"><i
+                  class="fas fa-edit"></i>Editar</a></td>
+
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>0012732</td>
+            <td>mayerly6757@gmail.com</td>
+            <td>Mayerly</td>
+            <td>Pirajan</td>
+            <td>8324932</td>
+            
+            <td>
+                <select class="estado_empleado">
+                    <option disabled>Seleccione un estado</option>
+                    <option>Activo</option>
+                    <option>Inactivo</option>
+                </select>
+            </td>
+            <td> <a href="" id="editar_empleado" 
+                class="link_editar_empleado"><i
+                  class="fas fa-edit"></i>Editar</a></td>
+          </tr>
+        </table>
     </div>
   </div>
     <!--------ver clientes ------->
