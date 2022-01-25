1c41c87ebd768d1bbc302ead4a9e09b588510e42
SMOKE <damesa722@misena.edu.co>
2022-01-25 18:59:44 -0500

    boton cerrar

--- proyecto php/registrar producto/index.php
+++ proyecto php/registrar producto/index.php
@@ -17,9 +17,9 @@
       <div class="tituloFerreteria">
       <img src="imag/este.png" alt="">
      <div class="salir">
-    <a href="../Login Ferreteria/index_login.php"><h2>Cerrar Sesion</h2></a>
+    
       <button><a href="../Login Ferreteria/index_login.php"><i class="fas fa-door-open"></i></a></button>
-     
+      <a href="../Login Ferreteria/index_login.php"><h2>Cerrar Sesion</h2></a>
     </div>
      
      
