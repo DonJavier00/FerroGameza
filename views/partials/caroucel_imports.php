fe4f7b0f4809065f705ef6b95533b1bdc29b4680
damesa722 <damesa722@misena.edu.co>
2022-02-16 16:17:41 +0100

    prueba cambio de rama

--- views/partials/caroucel_imports.php
+++ views/partials/caroucel_imports.php
@@ -1,10 +1,7 @@
 <div class="grande">
     <div class="contenedorGrande contenedor">
-
-   
       <div class="contenedor-principal">
         <button role="button" id="flecha-izquierda" class="flecha-izquierda"><i class="fas fa-angle-left"></i></button>
-
         <div class="contenedor-carousel">
           <div class="carousel">
          <!-- <div class="contenido_espacio">  </div>-->
@@ -13,142 +10,107 @@
               <h2>Gestionar Usuario</h2><br>
               <img src="imag/locksmith.svg" alt="">
               <br>
-
               <p>Agrega administradores, empleados y proveedores a tu base de datos </p>
-
               <nav class="menu">
                 <ul>
                   <b>
                     <li class="submenu"><a href="#"> Seleccionar<i class=" fas fa-caret-down"></i> </i></a>
                   </b>
-
+                </ul>
                   <ul>
                     <b>
                       <li><a href="#gestionar_administrador" class="btn-open">Administrador</a></li>
                       <li><a href="#gestionar_empleado" class="btn-open">Empleado</a></li>
-                      <li><a href="#gestionar_proveedor" class="btn-open">Proveedor</a></li> 
-                      
-            
+                      <li><a href="#gestionar_proveedor" class="btn-open">Proveedor</a></li>
                     </b>
-
                   </ul>
-                 
                   </li>
-                 
               </nav>
-
             </div>
-            
             <div class="contenido1">
               <br>
               <h2>Inventario</h2><br>
               <img src="imag/inventario.png" alt="">
               <br>
-
               <p>Ten el control de tus productos con todas sus características</p>
               <nav class="menu">
                 <ul>
                   <b>
                     <li class="submenu"><a href="#"> Seleccionar<i class=" fas fa-caret-down"></i> </i></a>
                   </b>
-
+                </ul>
                   <ul>
                     <b>
                       <li><a href="/FerroGameza/proyecto%20php/inventario/" class="btn-open">Ver Inventario</a></li>
 
-
                     </b>
-
-                  </ul>
-
+                  </ul
                   </li>
               </nav>
             </div>
-
             <div class="contenido1">
               <br>
               <h2>Gestionar Reporte</h2><br>
               <img src="imag/reporte.png" alt="">
               <br>
               <p>Genera los reportes necesarios de las ventas realizadas </p>
-
               <nav class="menu">
                 <ul>
                   <b>
                     <li class="submenu"><a href="#"> Seleccionar<i class=" fas fa-caret-down"></i> </i></a>
                   </b>
-
+                </ul>
                   <ul>
                     <b>
                       <li><a href="#gestionar_reportes" class="btn-open"> Ver Reporte</a></li>
-
                     </b>
-
                   </ul>
-
                   </li>
               </nav>
             </div>
-            
             <div class="contenido1">
               <br>
               <h2>Gestionar Factura</h2><br>
               <img src="imag/cuenta.svg" alt="">
               <br>
-
-
               <p>Genera tu factura de la mejor manera, rapido y efectivo </p>
-
-
               <nav class="menu">
                 <ul>
                   <b>
                     <li class="submenu"><a href="#"> Seleccionar<i class=" fas fa-caret-down"></i> </i></a>
                   </b>
-
+                </ul>
                   <ul>
                     <b>
                       <li><a href="#factura" class="btn-open">Factura </a></li>
                       <li><a href="#factura_cotizacion" class="btn-open">Cotizacion </a></li>
                       <li><a href="#factura_credito" class="btn-open">Credito</a></li>
                     </b>
-
                   </ul>
-
                   </li>
               </nav>
             </div>
-           
-
-   
             <div class="contenido1">
               <br>
               <h2>Registrar Producto</h2><br>
               <img src="imag/caja.png" alt="">
               <br>
               <p>Registra tus productos, compras y administra tu ingreso </p>
-              
               <nav class="menu">
                 <ul>
                   <b>
                     <li class="submenu"><a href="#"> Seleccionar<i class=" fas fa-caret-down"></i> </i></a>
                   </b>
-
+                </ul>
                   <ul>
                     <b>
                       <li><a href="#modal" class="btn-open">Nuevo </a></li>
                       <li><a href="#registrar_compra" class="btn-open">Reg Compra </a></li>
-
                     </b>
-
                   </ul>
-
                   </li>
               </nav>
-          
             </div>
-
-
             <div class="contenido1">
               <br>
               <h2>Usuarios</h2><br>
@@ -160,7 +122,7 @@
                   <b>
                     <li class="submenu"><a href="#"> Seleccionar<i class=" fas fa-caret-down"></i> </i></a>
                   </b>
-
+                </ul>
                   <ul>
                     <b>
                     <li><a href="#ver_administradores" class="btn-open">Ver administrador </a></li>
@@ -168,24 +130,18 @@
                       <li><a href="#ver_proveedores" class="btn-open">Ver proveedores </a></li>
                       <li><a href="#ver_clientes" class="btn-open">Ver clientes </a></li>
                     </b>
-
                   </ul>
-
                   </li>
               </nav>
             </div>
-
             <div class="contenido_espacio">  </div>
-           
           </div>
           <div class="espacio"></div>
           <button role="button" id="flecha-derecha" class="flecha-derecha"><i class="fas fa-angle-right"></i></button>
           <div class="estorbo">
       <img src="" alt="">
     </div>
-   
         </div>
-       
       </div>
      
 
